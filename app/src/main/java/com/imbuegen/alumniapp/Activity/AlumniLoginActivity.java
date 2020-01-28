961c4805e0fa2b1ba746a5e5f28586df8d1092b6
Raj Shah <rajshah2320@gmail.com>
2020-01-28 19:44:28 +0530

    Search and offline capabilities

--- app/src/main/java/com/imbuegen/alumniapp/Activity/AlumniLoginActivity.java
+++ app/src/main/java/com/imbuegen/alumniapp/Activity/AlumniLoginActivity.java
@@ -28,7 +28,8 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.imbuegen.alumniapp.Service.SFHandler;import com.imbuegen.alumniapp.R;
+import com.imbuegen.alumniapp.Service.SFHandler;
+import com.imbuegen.alumniapp.R;
 
 
 /**
